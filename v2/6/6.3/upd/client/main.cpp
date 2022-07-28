ba5bc8f956732e5b567591ab03599c0df2640447
RomantsovS <romancov123@gmail.com>
2022-07-28 20:52:09 +0300

    2022-07-28

--- v2/6/6.3/upd/client/main.cpp
+++ v2/6/6.3/upd/client/main.cpp
@@ -1,29 +1,29 @@
-#include <unistd.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <signal.h>
-#include <errno.h>
-#include <cstring>
+#include <unistd.h>
 
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#include <cstring>
 
 int main() {
     int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
-    if(sock == -1) {
+    if (sock == -1) {
         perror("failed to start socket\n");
         return 1;
     }
 
     sockaddr_in addr;
-    memset((char*)&addr, 0, sizeof(addr));
+    memset((char *)&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
     addr.sin_port = htons(7654);
-    if(inet_aton("127.0.0.1", &addr.sin_addr) == 0) {
+    if (inet_aton("127.0.0.1", &addr.sin_addr) == 0) {
         perror("iten_aton failed'n");
         return 1;
     }
@@ -31,16 +31,13 @@ int main() {
     const size_t buf_size = 100;
 
     char buf[buf_size];
-    while (read(0, buf, buf_size))
-    {
+    while (read(0, buf, buf_size)) {
         socklen_t socklen = sizeof(addr);
         int res = sendto(sock, buf, strlen(buf), 0, (sockaddr *)&addr, socklen);
-        if (res == -1)
-        {
+        if (res == -1) {
             perror("send error\n");
             return 1;
-        }
-        else
+        } else
             printf("sent %d bytes\n", res);
     }
 
